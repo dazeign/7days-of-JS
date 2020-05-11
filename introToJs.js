d6add1f20c0d85ddf7a038968e7b30bbceb9571b
Frankpeter Ani <frankpeterani@gmail.com>
2020-05-11 09:53:25 +0100

    Second comit

--- introToJs.js
+++ introToJs.js
@@ -2,6 +2,7 @@
 let firstName = 'Frankie';
 let lastName = 'Ani';
 
+const fullName = `${firstName} ${lastName}`;
 console.log(firstName + " " + lastName);
 
 
@@ -21,3 +22,24 @@ console.log(`${firstName} ${lastName}`.split(''));
 //.split(' ') outputs each string as an argument in an array
 //.length .split() .trim() are called string methods
  
+console.log(typeof firstName); //typeof will tell you the data type
+
+
+
+//numbers
+let example = 21.5;
+
+console.log(example);
+console.log(parseInt (example)); //will throw in the integer of the number
+console.log(parseFloat (example)); //will throw in the float value if there is one
+console.log(example.toFixed(5)); //will set the number in the function to 5 places exactly
+
+let example1 = parseInt("Hello 33 World 22");
+let example2 = parseFloat('44 Dylan 33');
+let example3 = 55.3333.toFixed(0);
+let example4 = 200.0.toFixed(2);
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
